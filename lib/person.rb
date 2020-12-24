5a9bbd708a830e59c29493db2d8408d801406df3
Roy Mosby <ego.madking@gmail.com>
2020-12-23 19:19:14 -0500

    commit tree 0dbc20011dd83ae81b555d757bd4c3f506321543

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
 class Person
   #your code here
+
+  def initialize(attribs)
+    attribs.each do |key, attrib|
+      self.class.attr_accessor(key)
+      self.send(("#{key}="), attrib)
+    end
+  end
 end
